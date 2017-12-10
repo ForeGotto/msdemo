a428c5e49eb216a10f10332c31f7e2d41a192634
fotg <fotg@foxmail.com>
2017-12-10 10:24:58 +0800

    Fix the bug that cipher text can't be deciphered twice

--- src/main/java/cipher/Encipher.java
+++ src/main/java/cipher/Encipher.java
@@ -2,41 +2,25 @@ package cipher;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
 public class Encipher {
 
-
     private String result;
-    private static KeyGenerator keyGenerator;
-    private static SecureRandom random;
-    private static Cipher cipher;
-
-    static {
-        random = new SecureRandom();
-        try {
-            cipher = Cipher.getInstance("AES/OFB8/PKCS5Padding");
-            keyGenerator = KeyGenerator.getInstance("AES");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        }
-    }
 
     public Encipher(String password, String plaintext) {
         try {
 
             plaintext = plaintext.substring(1, plaintext.length() - 1);
-//            System.out.println(plaintext);
 
-            random.setSeed(password.getBytes("UTF-8"));
+            SecureRandom random = new SecureRandom(password.getBytes("UTF-8"));
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
             keyGenerator.init(random);
             SecretKey key = keyGenerator.generateKey();
+
+            Cipher cipher = Cipher.getInstance("AES/OFB8/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, key);
 
             byte[] ivArray = cipher.getIV();
